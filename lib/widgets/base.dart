f57dfb94bf0739d228b846aa11b05f9a6a258cdc
Thiago Luigi <thiagoluigi7@hotmail.com>
2023-05-25 20:52:12 -0300

    fix(Widget): Finalized base widget

--- lib/widgets/base.dart
+++ lib/widgets/base.dart
@@ -1,12 +1,14 @@
-import 'package:obd_visualizer/modules/connection/connection.dart';
-
 import 'package:flutter/material.dart';
 
 class BaseWidget extends StatefulWidget {
-  Connection? connection;
-  int? value;
+  BaseWidget({
+    super.key,
+    required this.name,
+    required this.parsedValue
+  });
 
-  BaseWidget({super.key});
+  String name;
+  String parsedValue;
 
   @override
   BaseWidgetState createState() => BaseWidgetState();
@@ -16,24 +18,33 @@ class BaseWidgetState extends State<BaseWidget> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: 324,
-        height: 309,
-        child: Stack(children: <Widget>[
-          Positioned(
-              top: 0,
-              left: 0,
-              child: Container(
-                  width: 324,
-                  height: 309,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10),
-                    ),
-                    color: Color.fromRGBO(217, 217, 217, 1),
-                  ))),
-        ]));
+      width: 100,
+      height: 100,
+      child: Stack(alignment: Alignment.center,
+          children: <Widget>[
+        Positioned(
+          top: 0,
+          left: 0,
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              color: Color.fromRGBO(217, 217, 217, 1)))),
+        Column(
+          children: [
+            const SizedBox(height: 10),
+            Text(widget.name),
+            const SizedBox(height: 10),
+            Text(widget.parsedValue),
+            const SizedBox(height: 10)
+          ])
+      ]),
+    );
   }
 }
