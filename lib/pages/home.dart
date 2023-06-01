763f041e5ad959dc6463553617af8ab0cf996221
Thiago Luigi <thiagoluigi7@hotmail.com>
2023-06-01 11:35:19 -0300

    feat: :construction: Started the creation of a debug panel

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:obd_visualizer/widgets/engine_speed.dart';
-import 'package:obd_visualizer/widgets/fuel_level.dart';
-import 'package:obd_visualizer/widgets/vehicle_speed.dart';
+import 'package:obd_visualizer/widgets/engine_speed.widget.dart';
+import 'package:obd_visualizer/widgets/fuel_level.widget.dart';
+import 'package:obd_visualizer/widgets/vehicle_speed.widget.dart';
 
 import '../modules/connection/bluetooth_connection/bluetooth_connection.dart';
+import '../utils/globals.dart';
 import 'my_config_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -38,36 +41,111 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  setDebugMode(bool value) {
+    Globals.debugMode.value = value;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
             Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(children: <Widget>[
-                IconButton(
-                    icon: const Icon(Icons.bluetooth),
-                    tooltip: 'Get paired bluetooth device',
-                    padding: const EdgeInsets.all(50),
-                    onPressed: () => {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => MyConfigPage(title: widget.title, bt: widget.bt)),
-                      )
-                    }),
-              ]),
-            ]),
-            Row(crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    const Text('Debug'),
+                    ValueListenableBuilder<bool>(
+                        valueListenable: Globals.debugMode,
+                        builder: (context, value, Widget? child) {
+                          return Switch(
+                            value: value,
+                            onChanged: setDebugMode,
+                          );
+                        }
+                    ),
+                    const SizedBox(width: 500),
+                    IconButton(
+                      icon: const Icon(Icons.bluetooth),
+                      tooltip: 'Get paired bluetooth device',
+                      padding: const EdgeInsets.all(50),
+                      onPressed: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyConfigPage(
+                                  title: widget.title, bt: widget.bt)),
+                        )
+                      }),
+                  ]),
+                ]),
+            Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-              VehicleSpeed(bt: widget.bt),
-              EngineSpeed(bt: widget.bt),
-              FuelLevel(bt: widget.bt),
-            ]),
-      ])),
+                  VehicleSpeedWidget(bt: widget.bt),
+                  EngineSpeedWidget(bt: widget.bt),
+                  FuelLevelWidget(bt: widget.bt),
+                ]),
+            const SizedBox(height: 40),
+            ValueListenableBuilder<bool>(
+                valueListenable: Globals.debugMode,
+                builder: (context, value, Widget? child) {
+                  return Visibility(
+                    visible: value,
+                    child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child:
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text('Debug mode: ${Globals.debugMode.value.toString()}'),
+                              const SizedBox(width: 10),
+                              Text('Connected: ${widget.bt.connection?.isConnected.toString()}'),
+                              const SizedBox(width: 10),
+                              // TODO: Add pid being sent by info widget
+                              Text('AAAAAAAAAAAAAA'),
+                              // TODO: Add input sink and output sink
+                              // Text('Connected: ${widget.bt.connection?.isConnected.toString()}')
+                            ],
+                          )
+                    ),
+                  );
+                }),
+            // TODO: Add switches to disable mock for each info widget
+            // TODO: Add switch to enable and disable data retrieval periodically
+            ValueListenableBuilder<bool>(
+                valueListenable: Globals.debugMode,
+                builder: (context, value, Widget? child) {
+                  return Visibility(
+                    visible: value,
+                    child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child:
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Debug mode: ${Globals.debugMode.value.toString()}'),
+                            const SizedBox(width: 10),
+                            Text('Connected: ${widget.bt.connection?.isConnected.toString()}'),
+                            const SizedBox(width: 10),
+                            // TODO: Add pid being sent by widget
+                            Text('AAAAAAAAAAAAAA'),
+                            // TODO: Add input sink and output sink
+                            // Text('Connected: ${widget.bt.connection?.isConnected.toString()}')
+                          ],
+                        )
+                    ),
+                  );
+                })
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/utils/globals.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Globals {
+  static ValueNotifier<bool> debugMode = ValueNotifier<bool>(false);
+}
